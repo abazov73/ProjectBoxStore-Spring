5e05dd5f2a7764323cc1e3c9794f071f53910eb7
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-02-14 12:12:25 +0400

    Initial commit. Backend init.

---
+++ backend/ipLab/src/main/java/com/example/ipLab/IpLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ipLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpLabApplication.class, args);
+	}
+
+}
---
+++ backend/ipLab/src/test/java/com/example/ipLab/IpLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
